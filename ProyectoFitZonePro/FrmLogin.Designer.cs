561a8c10786f7b5d6de0cdebe40a8bcd28619b1d
Ulises E.G. <escobar.gonzalez.ulises@gmail.com>
2026-03-27 08:28:10 -0600

    avance login

--- ProyectoFitZonePro/FrmLogin.Designer.cs
+++ ProyectoFitZonePro/FrmLogin.Designer.cs
@@ -32,25 +32,30 @@
             this.TxtPassword = new System.Windows.Forms.TextBox();
             this.BtnIngresar = new System.Windows.Forms.Button();
             this.BtnSalir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // TxtUsuario
             // 
-            this.TxtUsuario.Location = new System.Drawing.Point(265, 129);
+            this.TxtUsuario.Location = new System.Drawing.Point(80, 104);
             this.TxtUsuario.Name = "TxtUsuario";
-            this.TxtUsuario.Size = new System.Drawing.Size(196, 30);
+            this.TxtUsuario.Size = new System.Drawing.Size(196, 26);
             this.TxtUsuario.TabIndex = 0;
             // 
             // TxtPassword
             // 
-            this.TxtPassword.Location = new System.Drawing.Point(265, 189);
+            this.TxtPassword.Location = new System.Drawing.Point(80, 164);
             this.TxtPassword.Name = "TxtPassword";
-            this.TxtPassword.Size = new System.Drawing.Size(196, 30);
+            this.TxtPassword.Size = new System.Drawing.Size(196, 26);
             this.TxtPassword.TabIndex = 1;
             // 
             // BtnIngresar
             // 
-            this.BtnIngresar.Location = new System.Drawing.Point(280, 240);
+            this.BtnIngresar.Location = new System.Drawing.Point(95, 215);
             this.BtnIngresar.Name = "BtnIngresar";
             this.BtnIngresar.Size = new System.Drawing.Size(164, 64);
             this.BtnIngresar.TabIndex = 2;
@@ -60,7 +65,7 @@
             // 
             // BtnSalir
             // 
-            this.BtnSalir.Location = new System.Drawing.Point(280, 310);
+            this.BtnSalir.Location = new System.Drawing.Point(95, 285);
             this.BtnSalir.Name = "BtnSalir";
             this.BtnSalir.Size = new System.Drawing.Size(164, 64);
             this.BtnSalir.TabIndex = 3;
@@ -68,21 +73,65 @@
             this.BtnSalir.UseVisualStyleBackColor = true;
             this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(166)))), ((int)(((byte)(121)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1080, 36);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BtnSalir);
+            this.panel2.Controls.Add(this.BtnIngresar);
+            this.panel2.Controls.Add(this.TxtPassword);
+            this.panel2.Controls.Add(this.TxtUsuario);
+            this.panel2.Location = new System.Drawing.Point(355, 117);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(370, 436);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Location = new System.Drawing.Point(351, 113);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(378, 444);
+            this.panel3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(993, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 409);
-            this.Controls.Add(this.BtnSalir);
-            this.Controls.Add(this.BtnIngresar);
-            this.Controls.Add(this.TxtPassword);
-            this.Controls.Add(this.TxtUsuario);
+            this.BackgroundImage = global::ProyectoFitZonePro.Properties.Resources.Recurso_1_4x;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1080, 670);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmLogin";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -92,6 +141,10 @@
         private System.Windows.Forms.TextBox TxtPassword;
         private System.Windows.Forms.Button BtnIngresar;
         private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProyectoFitZonePro/FrmLogin.cs
+++ ProyectoFitZonePro/FrmLogin.cs
@@ -1,7 +1,9 @@
-﻿using Manejadores;
+﻿using Entidades;
+using Manejadores;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
-using Entidades;
+using System.Drawing.Drawing2D;
 
 namespace ProyectoFitZonePro
 {
@@ -13,6 +15,7 @@ namespace ProyectoFitZonePro
         {
             InitializeComponent();
             Ml = new ManejadorLogin();
+            this.DoubleBuffered = true; //para eliminar los parpadeos, hace que lo dibuje en memoria antes de mostrarlo
 
             // Para que la contraseña salga con asteriscos (por si no lo configuraste en el diseño)
             TxtPassword.PasswordChar = '*';
@@ -64,6 +67,41 @@ namespace ProyectoFitZonePro
         private void BtnSalir_Click(object sender, EventArgs e)
         {
             Application.Exit(); // Cierra todo el programa por completo
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            int radioBorde = 5; // Puedes hacer este número más grande para más curva
+
+            // Creamos la ruta del tamaño exacto del panel
+            Rectangle areaPanel = new Rectangle(0, 0, panel2.Width, panel2.Height);
+            GraphicsPath rutaPanel = CrearRutaRedondeada(areaPanel, radioBorde);
+
+            // Aplicamos el recorte al panel
+            panel2.Region = new Region(rutaPanel);
+            panel3.Region = new Region(rutaPanel);
+        
+        panel3.BackColor = Color.FromArgb(40, Color.Black);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private GraphicsPath CrearRutaRedondeada(Rectangle rect, int radio)
+        {
+            GraphicsPath ruta = new GraphicsPath();
+            int diametro = radio * 2;
+
+            // Dibujamos los 4 arcos de las esquinas
+            ruta.AddArc(rect.X, rect.Y, diametro, diametro, 180, 90); // Arriba Izquierda
+            ruta.AddArc(rect.Right - diametro, rect.Y, diametro, diametro, 270, 90); // Arriba Derecha
+            ruta.AddArc(rect.Right - diametro, rect.Bottom - diametro, diametro, diametro, 0, 90); // Abajo Derecha
+            ruta.AddArc(rect.X, rect.Bottom - diametro, diametro, diametro, 90, 90); // Abajo Izquierda
+
+            ruta.CloseFigure(); // Cerramos la figura uniendo los arcos
+            return ruta;
         }
     }
 }
