11a14c04d23c562a72e4cb6d62b7d1e02ef26981
Ulises E.G. <escobar.gonzalez.ulises@gmail.com>
2026-05-08 09:38:36 -0600

    ahi ta su pto dashboard wey ahora pasame al botijon de lirroy, yo te di to mi love se fue corriento y no volvio mas

--- ProyectoFitZonePro/FrmDashboard.Designer.cs
+++ ProyectoFitZonePro/FrmDashboard.Designer.cs
@@ -28,23 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LblEstado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PSom = new System.Windows.Forms.Panel();
+            this.Pdel = new System.Windows.Forms.Panel();
+            this.PSom3 = new System.Windows.Forms.Panel();
+            this.Pdel3 = new System.Windows.Forms.Panel();
+            this.PSom2 = new System.Windows.Forms.Panel();
+            this.Pdel2 = new System.Windows.Forms.Panel();
+            this.PSom4 = new System.Windows.Forms.Panel();
+            this.Pdel4 = new System.Windows.Forms.Panel();
+            this.PSom5 = new System.Windows.Forms.Panel();
+            this.Pdel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.PSom.SuspendLayout();
+            this.Pdel.SuspendLayout();
+            this.PSom3.SuspendLayout();
+            this.Pdel3.SuspendLayout();
+            this.PSom2.SuspendLayout();
+            this.Pdel2.SuspendLayout();
+            this.PSom4.SuspendLayout();
+            this.Pdel4.SuspendLayout();
+            this.PSom5.SuspendLayout();
+            this.Pdel5.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // LblEstado
+            // 
+            this.LblEstado.AutoSize = true;
+            this.LblEstado.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEstado.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.LblEstado.Location = new System.Drawing.Point(9, 30);
+            this.LblEstado.Name = "LblEstado";
+            this.LblEstado.Size = new System.Drawing.Size(266, 16);
+            this.LblEstado.TabIndex = 18;
+            this.LblEstado.Text = "FitZone Pro sistema de administración";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 21);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Vista de miembros";
+            // 
+            // PSom
+            // 
+            this.PSom.Controls.Add(this.Pdel);
+            this.PSom.Location = new System.Drawing.Point(28, 80);
+            this.PSom.Name = "PSom";
+            this.PSom.Size = new System.Drawing.Size(258, 179);
+            this.PSom.TabIndex = 19;
+            // 
+            // Pdel
+            // 
+            this.Pdel.BackColor = System.Drawing.Color.White;
+            this.Pdel.Controls.Add(this.label7);
+            this.Pdel.Controls.Add(this.label1);
+            this.Pdel.Location = new System.Drawing.Point(4, 4);
+            this.Pdel.Name = "Pdel";
+            this.Pdel.Size = new System.Drawing.Size(250, 171);
+            this.Pdel.TabIndex = 9;
+            // 
+            // PSom3
+            // 
+            this.PSom3.Controls.Add(this.Pdel3);
+            this.PSom3.Location = new System.Drawing.Point(610, 80);
+            this.PSom3.Name = "PSom3";
+            this.PSom3.Size = new System.Drawing.Size(258, 179);
+            this.PSom3.TabIndex = 19;
+            // 
+            // Pdel3
+            // 
+            this.Pdel3.BackColor = System.Drawing.Color.White;
+            this.Pdel3.Controls.Add(this.label9);
+            this.Pdel3.Controls.Add(this.label4);
+            this.Pdel3.Location = new System.Drawing.Point(4, 4);
+            this.Pdel3.Name = "Pdel3";
+            this.Pdel3.Size = new System.Drawing.Size(250, 171);
+            this.Pdel3.TabIndex = 9;
+            // 
+            // PSom2
+            // 
+            this.PSom2.Controls.Add(this.Pdel2);
+            this.PSom2.Location = new System.Drawing.Point(319, 80);
+            this.PSom2.Name = "PSom2";
+            this.PSom2.Size = new System.Drawing.Size(258, 179);
+            this.PSom2.TabIndex = 19;
+            // 
+            // Pdel2
+            // 
+            this.Pdel2.BackColor = System.Drawing.Color.White;
+            this.Pdel2.Controls.Add(this.label8);
+            this.Pdel2.Controls.Add(this.label2);
+            this.Pdel2.Location = new System.Drawing.Point(4, 4);
+            this.Pdel2.Name = "Pdel2";
+            this.Pdel2.Size = new System.Drawing.Size(250, 171);
+            this.Pdel2.TabIndex = 9;
+            // 
+            // PSom4
+            // 
+            this.PSom4.Controls.Add(this.Pdel4);
+            this.PSom4.Location = new System.Drawing.Point(28, 284);
+            this.PSom4.Name = "PSom4";
+            this.PSom4.Size = new System.Drawing.Size(548, 284);
+            this.PSom4.TabIndex = 19;
+            // 
+            // Pdel4
+            // 
+            this.Pdel4.BackColor = System.Drawing.Color.White;
+            this.Pdel4.Controls.Add(this.label5);
+            this.Pdel4.Location = new System.Drawing.Point(4, 4);
+            this.Pdel4.Name = "Pdel4";
+            this.Pdel4.Size = new System.Drawing.Size(540, 276);
+            this.Pdel4.TabIndex = 9;
+            // 
+            // PSom5
+            // 
+            this.PSom5.Controls.Add(this.Pdel5);
+            this.PSom5.Location = new System.Drawing.Point(610, 284);
+            this.PSom5.Name = "PSom5";
+            this.PSom5.Size = new System.Drawing.Size(258, 284);
+            this.PSom5.TabIndex = 19;
+            // 
+            // Pdel5
+            // 
+            this.Pdel5.BackColor = System.Drawing.Color.White;
+            this.Pdel5.Controls.Add(this.label6);
+            this.Pdel5.Location = new System.Drawing.Point(4, 4);
+            this.Pdel5.Name = "Pdel5";
+            this.Pdel5.Size = new System.Drawing.Size(250, 276);
+            this.Pdel5.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Miembros totales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(30, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Activos hoy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(30, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Venta semanal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(15, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(261, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Entradas / salidas recientes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(15, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 40);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Distribucion de \r\nmembresias";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 80);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "label7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "label7";
             // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(895, 611);
+            this.Controls.Add(this.PSom2);
+            this.Controls.Add(this.PSom3);
+            this.Controls.Add(this.PSom5);
+            this.Controls.Add(this.PSom4);
+            this.Controls.Add(this.PSom);
+            this.Controls.Add(this.LblEstado);
+            this.Controls.Add(this.label3);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = " ";
+            this.Load += new System.EventHandler(this.FrmDashboard_Load);
+            this.PSom.ResumeLayout(false);
+            this.Pdel.ResumeLayout(false);
+            this.Pdel.PerformLayout();
+            this.PSom3.ResumeLayout(false);
+            this.Pdel3.ResumeLayout(false);
+            this.Pdel3.PerformLayout();
+            this.PSom2.ResumeLayout(false);
+            this.Pdel2.ResumeLayout(false);
+            this.Pdel2.PerformLayout();
+            this.PSom4.ResumeLayout(false);
+            this.Pdel4.ResumeLayout(false);
+            this.Pdel4.PerformLayout();
+            this.PSom5.ResumeLayout(false);
+            this.Pdel5.ResumeLayout(false);
+            this.Pdel5.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LblEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel PSom;
+        private System.Windows.Forms.Panel Pdel;
+        private System.Windows.Forms.Panel PSom3;
+        private System.Windows.Forms.Panel Pdel3;
+        private System.Windows.Forms.Panel PSom2;
+        private System.Windows.Forms.Panel Pdel2;
+        private System.Windows.Forms.Panel PSom4;
+        private System.Windows.Forms.Panel Pdel4;
+        private System.Windows.Forms.Panel PSom5;
+        private System.Windows.Forms.Panel Pdel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
     }
 }
--- ProyectoFitZonePro/FrmDashboard.cs
+++ ProyectoFitZonePro/FrmDashboard.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,50 @@ namespace ProyectoFitZonePro
         {
             InitializeComponent();
             md = new ManejadorDashboard();
+        }
+
+        private void FrmDashboard_Load(object sender, EventArgs e)
+        {
+            int radioBorde = 5;
+            GraphicsPath rutaPanel1 = CrearRutaRedondeada(new Rectangle(0, 0, Pdel.Width, Pdel.Height), radioBorde);
+            GraphicsPath rutaPanel2 = CrearRutaRedondeada(new Rectangle(0, 0, PSom.Width, PSom.Height), radioBorde);
+            GraphicsPath rutaPanel3 = CrearRutaRedondeada(new Rectangle(0, 0, PSom4.Width, PSom4.Height), radioBorde);
+            GraphicsPath rutaPanel4 = CrearRutaRedondeada(new Rectangle(0, 0, Pdel4.Width, Pdel4.Height), radioBorde);
+            GraphicsPath rutaPanel5 = CrearRutaRedondeada(new Rectangle(0, 0, PSom5.Width, PSom5.Height), radioBorde);
+            GraphicsPath rutaPanel6 = CrearRutaRedondeada(new Rectangle(0, 0, Pdel5.Width, Pdel5.Height), radioBorde);
+
+
+            Pdel.Region = new Region(rutaPanel1);
+            Pdel2.Region = new Region(rutaPanel1);
+            Pdel3.Region = new Region(rutaPanel1);
+            Pdel4.Region = new Region(rutaPanel4);
+            Pdel5.Region = new Region(rutaPanel6);
+
+            PSom.Region = new Region(rutaPanel2);
+            PSom2.Region = new Region(rutaPanel2);
+            PSom3.Region = new Region(rutaPanel2);
+            PSom4.Region = new Region(rutaPanel3);
+            PSom5.Region = new Region(rutaPanel5);
+
+            PSom.BackColor = Color.FromArgb(20, Color.Black);
+            PSom2.BackColor = Color.FromArgb(20, Color.Black);
+            PSom3.BackColor = Color.FromArgb(20, Color.Black);
+            PSom4.BackColor = Color.FromArgb(20, Color.Black);
+            PSom5.BackColor = Color.FromArgb(20, Color.Black);
+        }
+        private GraphicsPath CrearRutaRedondeada(Rectangle rect, int radio)
+        {
+            GraphicsPath ruta = new GraphicsPath();
+            int diametro = radio * 2;
+
+            // Dibujamos los 4 arcos de las esquinas
+            ruta.AddArc(rect.X, rect.Y, diametro, diametro, 180, 90); // Arriba Izquierda
+            ruta.AddArc(rect.Right - diametro, rect.Y, diametro, diametro, 270, 90); // Arriba Derecha
+            ruta.AddArc(rect.Right - diametro, rect.Bottom - diametro, diametro, diametro, 0, 90); // Abajo Derecha
+            ruta.AddArc(rect.X, rect.Bottom - diametro, diametro, diametro, 90, 90); // Abajo Izquierda
+
+            ruta.CloseFigure(); // Cerramos la figura uniendo los arcos
+            return ruta;
         }
     }
 }
