a20467f18e5b47c8fd296148ce669b5c82f3f4d5
Ulises E.G. <escobar.gonzalez.ulises@gmail.com>
2026-05-08 20:34:08 -0600

    avance al de mantenimientos, a ver si no vale kk

--- ProyectoFitZonePro/FrmDatosEquiposMantenimiento.Designer.cs
+++ ProyectoFitZonePro/FrmDatosEquiposMantenimiento.Designer.cs
@@ -28,51 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BtnCancelar = new System.Windows.Forms.Button();
-            this.BtnAceptar = new System.Windows.Forms.Button();
             this.TxtNombre = new System.Windows.Forms.TextBox();
             this.DtpFechaAdquisicion = new System.Windows.Forms.DateTimePicker();
             this.BtnFinalizar = new System.Windows.Forms.Button();
             this.TxtIdEquipo = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.LblEstado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // BtnCancelar
-            // 
-            this.BtnCancelar.Location = new System.Drawing.Point(292, 281);
-            this.BtnCancelar.Name = "BtnCancelar";
-            this.BtnCancelar.Size = new System.Drawing.Size(149, 72);
-            this.BtnCancelar.TabIndex = 22;
-            this.BtnCancelar.Text = "Cancelar";
-            this.BtnCancelar.UseVisualStyleBackColor = true;
-            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
-            // 
-            // BtnAceptar
-            // 
-            this.BtnAceptar.Location = new System.Drawing.Point(119, 281);
-            this.BtnAceptar.Name = "BtnAceptar";
-            this.BtnAceptar.Size = new System.Drawing.Size(149, 72);
-            this.BtnAceptar.TabIndex = 21;
-            this.BtnAceptar.Text = "Aceptar";
-            this.BtnAceptar.UseVisualStyleBackColor = true;
-            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
             // 
             // TxtNombre
             // 
-            this.TxtNombre.Location = new System.Drawing.Point(70, 108);
+            this.TxtNombre.Location = new System.Drawing.Point(28, 223);
             this.TxtNombre.Name = "TxtNombre";
-            this.TxtNombre.Size = new System.Drawing.Size(210, 30);
+            this.TxtNombre.Size = new System.Drawing.Size(210, 26);
             this.TxtNombre.TabIndex = 18;
             // 
             // DtpFechaAdquisicion
             // 
-            this.DtpFechaAdquisicion.Location = new System.Drawing.Point(322, 62);
+            this.DtpFechaAdquisicion.Location = new System.Drawing.Point(261, 128);
             this.DtpFechaAdquisicion.Name = "DtpFechaAdquisicion";
-            this.DtpFechaAdquisicion.Size = new System.Drawing.Size(321, 30);
+            this.DtpFechaAdquisicion.Size = new System.Drawing.Size(286, 26);
             this.DtpFechaAdquisicion.TabIndex = 27;
             // 
             // BtnFinalizar
             // 
-            this.BtnFinalizar.Location = new System.Drawing.Point(109, 189);
+            this.BtnFinalizar.Location = new System.Drawing.Point(52, 255);
             this.BtnFinalizar.Name = "BtnFinalizar";
             this.BtnFinalizar.Size = new System.Drawing.Size(149, 42);
             this.BtnFinalizar.TabIndex = 29;
@@ -82,37 +74,171 @@
             // 
             // TxtIdEquipo
             // 
-            this.TxtIdEquipo.Location = new System.Drawing.Point(70, 62);
+            this.TxtIdEquipo.Location = new System.Drawing.Point(13, 128);
             this.TxtIdEquipo.Name = "TxtIdEquipo";
-            this.TxtIdEquipo.Size = new System.Drawing.Size(210, 30);
+            this.TxtIdEquipo.Size = new System.Drawing.Size(210, 26);
             this.TxtIdEquipo.TabIndex = 30;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 515);
+            this.panel1.TabIndex = 31;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.TxtIdEquipo);
+            this.panel3.Controls.Add(this.BtnFinalizar);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.DtpFechaAdquisicion);
+            this.panel3.Controls.Add(this.TxtNombre);
+            this.panel3.Location = new System.Drawing.Point(2, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(590, 511);
+            this.panel3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(290, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(166)))), ((int)(((byte)(121)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(117, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Control;
+            this.panel4.Controls.Add(this.LblEstado);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(590, 67);
+            this.panel4.TabIndex = 7;
+            // 
+            // LblEstado
+            // 
+            this.LblEstado.AutoSize = true;
+            this.LblEstado.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEstado.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.LblEstado.Location = new System.Drawing.Point(10, 35);
+            this.LblEstado.Name = "LblEstado";
+            this.LblEstado.Size = new System.Drawing.Size(488, 17);
+            this.LblEstado.TabIndex = 21;
+            this.LblEstado.Text = "Complementa la información  para el mantenimiento del equipo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(364, 25);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Registrar / Editar mantenimiento";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel2.Location = new System.Drawing.Point(8, 8);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(594, 515);
+            this.panel2.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Id del equipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 20);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Fecha del mantenimiento programado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Nombre";
             // 
             // FrmDatosEquiposMantenimiento
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(655, 443);
-            this.Controls.Add(this.TxtIdEquipo);
-            this.Controls.Add(this.BtnFinalizar);
-            this.Controls.Add(this.DtpFechaAdquisicion);
-            this.Controls.Add(this.BtnCancelar);
-            this.Controls.Add(this.BtnAceptar);
-            this.Controls.Add(this.TxtNombre);
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.ClientSize = new System.Drawing.Size(1013, 601);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmDatosEquiposMantenimiento";
             this.Text = "FrmDatosEquiposMantenimiento";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button BtnCancelar;
-        private System.Windows.Forms.Button BtnAceptar;
         private System.Windows.Forms.TextBox TxtNombre;
         private System.Windows.Forms.DateTimePicker DtpFechaAdquisicion;
         private System.Windows.Forms.Button BtnFinalizar;
         private System.Windows.Forms.TextBox TxtIdEquipo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label LblEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
     }
 }
